7bb5a460c2ad5f4f4ab69a79737fc1d3b4100e89
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-08-18 14:31:34 +0200

    added ClientsStorage Service

--- src/app/services/clientsStorageService/clients-storage.service.ts
+++ src/app/services/clientsStorageService/clients-storage.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ClientsWrapper } from 'src/app/model/ClientsWrapper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsStorageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+   }
+
+  uri = 'https://localhost:44334/api/ClientsStorage';
+
+  public LoadClients(): Observable<ClientsWrapper>{
+    return this.http.get<ClientsWrapper>(this.uri);
+  }
+
+  postClientData(clients: ClientsWrapper): Observable<ClientsWrapper> {
+    return this.http.post<ClientsWrapper>(this.uri, clients);
+  }
 }
