e3afc80b17ea81dae1683cd61efd06aebe4d57bf
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-08-19 15:13:43 +0200

    added some styling to the buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientsManagerComponent } from './clients-manager/clients-manager.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path:'clients', component: ClientsManagerComponent}
